0e9d89af559af6e039fe063d7b201359a4864015
itgsod-daniel-wallen <daniel.wallen@elev.it-gymnasiet.se>
2014-09-23 14:49:20 +0200

    ticket_price

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -3,3 +3,14 @@ def ask_age
   age = gets.to_i
 end
 
+def ticket_price(ask_age)
+  price = 0
+  if ask_age <=17
+    price = 10
+  elsif ask_age <=64
+    price = 20
+  else
+    price = 15
+  end
+  return price
+end
