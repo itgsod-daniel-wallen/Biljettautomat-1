1a09320b48d78981a231b3f4e156d3c07b1d043b
itgsod-daniel-wallen <daniel.wallen@elev.it-gymnasiet.se>
2014-09-30 14:17:27 +0200

    made the code fancy

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,17 +1,17 @@
 def ask_age
   puts "Enter your age"
-  age = gets.to_i
-  return age
+  return gets.to_i
 end
 
-def ticket_price(ask_age)
-  price = 0
-  if ask_age <=17
+def ticket_price(age)
+  if age <= 17
     price = 10
-  elsif ask_age <=64
+  elsif age <= 64
     price = 20
   else
     price = 15
   end
   return price
 end
+
+puts "The price is #{ticket_price(ask_age)}kr!"
